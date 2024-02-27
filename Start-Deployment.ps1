a48432de3940ea14daca2b027dc127a1756228fe
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-02-27 19:26:50 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -1,6 +1,17 @@
+# Cloud OSD ZTI Script
+# Written by LZV
+
+# Variables
+$osVersion = "Windows 11"
+$osBuild = "23H2"
+$osLanguage = "en-us"
+$osEdition = "Enterprise"
+$osActivation = "Volume"
+
 Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
 Start-Sleep -Seconds 5
-Write-Host -ForegroundColor Red "Please remove USB From computer NOW - Press Enter when done"
+
+Write-Host -ForegroundColor Red "If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected. When ready to continue press ENTER"
 pause
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume
+Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation
