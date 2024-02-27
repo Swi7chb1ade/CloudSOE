045fef23a345da0022e176245eb67bea34c25f19
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-02-27 21:47:03 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -1,18 +1,27 @@
 # Cloud OSD ZTI Script
 # Written by LZV
 
-# Variables
+# Paramaters for Start-OSDCloud Command
 $osVersion = "Windows 11"
 $osBuild = "23H2"
 $osLanguage = "en-us"
 $osEdition = "Enterprise"
 $osActivation = "Volume"
 
+# Start Message
 Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
 Start-Sleep -Seconds 5
 
-Write-Host -ForegroundColor Red "If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected."
-Write-Host -ForegroundColor Red "Continuing after this point is destructive and automatic - press ENTER if you are sure you want to wipe the SSD and install Windows on  this computer"
+# Warning Message
+Write-Host -ForegroundColor Red "############################################################################################################################"
+Write-Host -ForegroundColor Red "#                                                      WARNING WARNING WARNING                                             #"
+Write-Host -ForegroundColor Red "# If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected.        #"
+Write-Host -ForegroundColor Red "# Continuing after this point is destructive and automatic - the SSD will be automatically wiped and Windows installed     #"
+Write-Host -ForegroundColor Red "# Press ENTER if you are sure you want to wipe the computer and install Windows                                            #"
+Write-Host -ForegroundColor Red "############################################################################################################################"
+
+# Pause to wait for user to confirm 
 pause
 
-Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -Firmware -Restart -ZTI
+# SENDIT
+Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -Restart -ZTI
