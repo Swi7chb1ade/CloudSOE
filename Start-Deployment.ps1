4bba46abc8845dd908e5f41a040d7c43a30ab490
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-02-27 19:16:49 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -1,4 +1,6 @@
 Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
 Start-Sleep -Seconds 5
+Write-Host -ForegroundColor Red "Please remove USB From computer NOW - Press Enter when done"
+pause
 
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume
