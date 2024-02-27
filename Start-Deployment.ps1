9765aca82ffe5c8ba5e022756228f0c3b8f14dda
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-02-27 21:35:03 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -11,7 +11,8 @@ $osActivation = "Volume"
 Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
 Start-Sleep -Seconds 5
 
-Write-Host -ForegroundColor Red "If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected. When ready to continue press ENTER"
+Write-Host -ForegroundColor Red "If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected."
+Write-Host -ForegroundColor Red "Continuing after this point is destructive and automatic - press ENTER if you are sure you want to wipe the SSD and install Windows on  this computer"
 pause
 
-Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -Firmware -Restart
+Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -Firmware -Restart -ZTI
