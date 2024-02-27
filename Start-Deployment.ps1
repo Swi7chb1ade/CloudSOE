ca623b0cd20cc4a291d5dae4c64943819c16acb4
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-02-27 19:48:18 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -14,4 +14,4 @@ Start-Sleep -Seconds 5
 Write-Host -ForegroundColor Red "If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected. When ready to continue press ENTER"
 pause
 
-Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation
+Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -Firmware -Restart
