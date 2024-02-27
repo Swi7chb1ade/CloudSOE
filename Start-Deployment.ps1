57b2e656e73682b664c731281dd9e5219b424030
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-02-27 19:06:49 +1100

    Add files via upload

---
+++ Start-Deployment.ps1
@@ -0,0 +1,4 @@
+Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
+Start-Sleep -Seconds 5
+
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume
