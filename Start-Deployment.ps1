7e99614be573a0f9bd98fc373978447ced6f3e24
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-03-06 21:22:56 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -13,12 +13,14 @@ Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
 Start-Sleep -Seconds 5
 
 # Warning Message
-Write-Host -ForegroundColor Red "############################################################################################################################"
-Write-Host -ForegroundColor Red "#                                                      WARNING WARNING WARNING                                             #"
-Write-Host -ForegroundColor Red "# If you booted using Ventoy, remove the USB From the computer or things will break - otherwise leave it connected.        #"
-Write-Host -ForegroundColor Red "# Continuing after this point is destructive and automatic - the SSD will be automatically wiped and Windows installed     #"
-Write-Host -ForegroundColor Red "# Press ENTER if you are sure you want to wipe the computer and install Windows                                            #"
-Write-Host -ForegroundColor Red "############################################################################################################################"
+Write-Host -ForegroundColor Red "#################################################################################"
+Write-Host -ForegroundColor Red "#                         WARNING WARNING WARNING                               #"
+Write-Host -ForegroundColor Red "# If you booted using Ventoy, remove the USB From the computer or things        #"
+Write-Host -ForegroundColor Red "# will break - otherwise leave it connected.                                    #"
+Write-Host -ForegroundColor Red "# Continuing after this point is destructive and automatic                      #"
+Write-Host -ForegroundColor Red "# the SSD will be automatically wiped and Windows installed                     #"
+Write-Host -ForegroundColor Red "# Press ENTER if you are sure you want to wipe the computer and install Windows #"
+Write-Host -ForegroundColor Red "#################################################################################"
 
 # Pause to wait for user to confirm 
 pause
