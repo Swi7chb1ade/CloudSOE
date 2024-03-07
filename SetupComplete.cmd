51a728d0e3418097605890d93afa5fb693ab678f
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-03-07 17:23:41 +1100

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -1,3 +1,3 @@
 :: SetupComplete file for Windows 11
 powershell.exe -command Set-ExecutionPolicy Unrestricted -Force
-powershell.exe -command "& {IEX (IRM https://)}
+powershell.exe -command "& {IEX (IRM https://raw.githubusercontent.com/Swi7chb1ade/CloudSOE/main/Start-OOBE.ps1)}
