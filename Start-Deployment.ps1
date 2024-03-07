2c46e2f3442db04b56250a7df1932ee94f9259ba
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-03-07 17:19:12 +1100

    Update Start-Deployment.ps1

--- Start-Deployment.ps1
+++ Start-Deployment.ps1
@@ -1,5 +1,24 @@
-# Cloud OSD ZTI Script
-# Written by LZV
+<#
+.SYNOPSIS
+    Script for use with OSDCloud to deploy Windows 11 Enterprise. 
+    This script will automatically wipe the device, install Windows 11 Enterprise and copy an unattend.xml file once the install has completed.
+    The unattend.xml file will entierly skip the OOBE process, so the end user only has to confirm to the warning relating to data destruction
+.FUNCTIONALITY
+    OSDCloud Kick off script and finishing touches 
+.NOTES
+    Author: LZV
+    Last Edit: 2024-03-07
+    Version 0.1 - Creation (Alpha)
+#>
+
+# Determine if Desktop or Mobile computer
+$hardwareType = (Get-Computerinfo).CsPCSystemType
+if($hardwareType -eq "Desktop"){
+    $prefix = "PC"
+}
+elseif($hardwareType -eq "Mobile"){
+    $prefix = "NB"
+}
 
 # Paramaters for Start-OSDCloud Command
 $osVersion = "Windows 11"
@@ -9,7 +28,7 @@ $osEdition = "Enterprise"
 $osActivation = "Volume"
 
 # Unattend location
-$unattendUrl = "https://raw.githubusercontent.com/Swi7chb1ade/cloudosd/main/unattend.xml"
+$unattendUrl = "https://raw.githubusercontent.com/Swi7chb1ade/CloudSOE/main/unattend.xml"
 
 # Start Message
 Write-Host -ForegroundColor Green "Starting ABL OSDCloud Zero Touch Install"
@@ -21,18 +40,36 @@ $userResponse = $warningShell.Popup("If you booted using Ventory, remove the USB
 
 # If user clicks OK, begin process
 if($userResponse -eq 1){
+    # Gather asset number
+    # Prompt for Asset Tag Number
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    $title = "Computer Name"
+    $message   = 'Enter the computer asset number:'
+    $assetNumber = [Microsoft.VisualBasic.Interaction]::InputBox($message, $title)
 
-# Start OSDCloud Installation
-Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -ZTI
+    # Create Asset Name variable
+    $assetName = $prefix + $assetNumber
+    Write-Host -ForegroundColor Green "Starting OSDCloud deployment for $assetName"
 
-# Copy Unattend File
-Invoke-WebRequest $unattendUrl -outFile "C:\Windows\Panther\unattend.xml"
+    # Start OSDCloud Installation
+    Start-OSDCloud -OSVersion $osVersion -OSBuild $osBuild -OSLanguage $osLanguage -OSEdition $osEdition -OSActivation $osActivation -ZTI
 
-# Restart the computer once completed 
-Restart-Computer -Force
+    # Copy Unattend File    
+    Invoke-WebRequest $unattendUrl -outFile "C:\Windows\Panther\unattend.xml"
+
+    # Create text file containing asset number
+    $assetName | Out-File "C:\OSDCloud\AssetName.txt"
+
+    # Sleep for 30 secnods
+    Write-Host -ForegroundColor Green "OSDCloud deployment for $assetName done - Rebooting in 30 seconds"
+    Start-Sleep -Seconds 30
+
+    # Restart the computer once completed 
+    Restart-Computer -Force
 }
 
 # If user clicks CANCEL, stop process
 else {
+    Write-Host -ForegroundColor Red "Aborting OSDCloud Deployment!!!!"
     exit 1
 }
