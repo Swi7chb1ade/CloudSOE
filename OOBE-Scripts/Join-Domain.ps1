c9095e3a7a54ed11b78f3857ca691ce6c6cad2c5
Luke <102341038+Swi7chb1ade@users.noreply.github.com>
2024-03-07 19:42:53 +1100

    Update Join-Domain.ps1

--- OOBE-Scripts/Join-Domain.ps1
+++ OOBE-Scripts/Join-Domain.ps1
@@ -1,5 +1,6 @@
 # Join Domain
 
 $assetName = Get-Content "C:\OSDCloud\AssetName.txt"
+Rename-Computer -NewName $assetName
 
-Add-Computer -NewName $assetName -DomainName "abl.com.au" 
+#Add-Computer -NewName $assetName -DomainName "abl.com.au" 
